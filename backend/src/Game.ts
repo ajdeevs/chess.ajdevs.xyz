390dee87be4b66dc2f4f4d00c529c221a6b22bf9
ajdevs <bhawnaniankit@gmail.com>
2025-07-27 11:49:04 +0530

    backend v-1

---
+++ backend/src/Game.ts
@@ -0,0 +1,72 @@
+import { Chess } from "chess.js";
+import { WebSocket } from "ws";
+import { GAME_OVER, INIT_GAMES, MOVE } from "./messages";
+
+export class Game {
+  public player1: WebSocket;
+  public player2: WebSocket;
+  public board: Chess;
+  private movecount = 0;
+  private starttme: Date;
+
+  constructor(player1: WebSocket, player2: WebSocket) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.board = new Chess();
+    this.starttme = new Date();
+    this.movecount = 0;
+
+    this.player1.send(
+      JSON.stringify({ type: INIT_GAMES, payload: { color: "white" } }),
+    );
+    this.player2.send(
+      JSON.stringify({ type: INIT_GAMES, payload: { color: "black" } }),
+    );
+  }
+
+  makeMove(socket: WebSocket, move: { from: "string"; to: "string" }) {
+    if (this.movecount % 2 === 0 && socket !== this.player1) {
+      return;
+    }
+    if (this.movecount % 2 === 1 && socket !== this.player2) {
+      return;
+    }
+
+    try {
+      this.board.move(move);
+      this.movecount++;
+    } catch (e) {
+      return;
+    }
+
+    if (this.board.isGameOver()) {
+      //
+      this.player1.emit(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            winner: this.board.turn() == "w" ? "black" : "white",
+          },
+        }),
+      );
+      this.player2.emit(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            winner: this.board.turn() == "w" ? "black" : "white",
+          },
+        }),
+      );
+
+      return;
+    }
+
+    if (this.movecount % 2 === 0) {
+      this.player1.send(JSON.stringify({ type: MOVE, payload: move }));
+    } else {
+      this.player2.send(JSON.stringify({ type: MOVE, payload: move }));
+    }
+    //push the move
+    //send the update to the other player
+  }
+}
---
+++ backend/src/messages.ts
@@ -0,0 +1,3 @@
+export const INIT_GAMES = "init_games";
+export const MOVE = "move";
+export const GAME_OVER = "game_over";
