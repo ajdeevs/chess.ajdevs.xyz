62700984b81b0820b158ea0a9858b3b2943adc20
ajdevs <bhawnaniankit@gmail.com>
2025-07-27 23:36:47 +0530

    vercel.json added

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 export const useSocket = () => {
   const [socket, setSocket] = useState<WebSocket | null>(null);
-  const WS_URL = "ws://localhost:8080";
+  const WS_URL = import.meta.env.VITE_WS_URL;
 
   useEffect(() => {
     const ws = new WebSocket(WS_URL);
